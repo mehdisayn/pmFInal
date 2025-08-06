c6e43408bb720a074bb5fb32dc99b8d961ab09c2
mehdisayn <m21gits@gmail.com>
2025-08-07 04:46:14 +0600

    updated Dashboard

--- src/main/java/dsit/pmfinal/meddy/ceo/controller/CeoDashboardController.java
+++ src/main/java/dsit/pmfinal/meddy/ceo/controller/CeoDashboardController.java
@@ -12,39 +12,75 @@ public class CeoDashboardController
     public void initialize() {
     }
 
-    @javafx.fxml.FXML
+    @Deprecated
     public void feedbackReviewOnAction(ActionEvent actionEvent) {
     }
 
-    @javafx.fxml.FXML
+    @Deprecated
     public void complianceReportOnAction(ActionEvent actionEvent) {
     }
 
-    @javafx.fxml.FXML
+    @Deprecated
     public void performanceMetricsOnAction(ActionEvent actionEvent) {
     }
 
-    @javafx.fxml.FXML
+    @Deprecated
     public void departmentOverviewOnAction(ActionEvent actionEvent) {
     }
 
-    @javafx.fxml.FXML
+    @Deprecated
     public void strategicPlanningOnAction(ActionEvent actionEvent) {
     }
 
-    @javafx.fxml.FXML
+    @Deprecated
     public void finalApprovalOnAction(ActionEvent actionEvent) {
     }
 
-    @javafx.fxml.FXML
+    @Deprecated
     public void logoutOnAction(ActionEvent actionEvent) {
     }
 
-    @javafx.fxml.FXML
+    @Deprecated
     public void productionReportOnAction(ActionEvent actionEvent) {
     }
 
-    @javafx.fxml.FXML
+    @Deprecated
     public void financialSummaryOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnViewProduction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnLogout(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnDepartmentPerformance(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnApproveShifts(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnReviewMaintenance(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnReviewLogistics(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnReviewHRDecisions(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnApproveBudget(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnApproveSalaries(ActionEvent actionEvent) {
     }
 }
