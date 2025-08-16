7fc55cec56b792fcd24729229c884a24264a150b
ridwanHasan-khandakar <ridwan_rahin@proton.me>
2025-08-16 13:03:24 +0600

    Did work on PurchaseApprovalsController+PurchaseApprovals

--- src/main/java/dsit/pmfinal/rrhin/accountant/Controller/PurchaseApprovalsController.java
+++ src/main/java/dsit/pmfinal/rrhin/accountant/Controller/PurchaseApprovalsController.java
@@ -1,7 +1,11 @@
 package dsit.pmfinal.rrhin.accountant.Controller;
 
+import dsit.pmfinal.common_files.Jumper;
 import dsit.pmfinal.rrhin.accountant.model.PurchaseApprovals;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
@@ -24,24 +28,49 @@ public class PurchaseApprovalsController
     @javafx.fxml.FXML
     private TableColumn <PurchaseApprovals,String> itemNameCol;
 
+    //dummy list
+    private ObservableList<PurchaseApprovals> purchaseApprovalsObservableList = FXCollections.observableArrayList();
+
+
     @javafx.fxml.FXML
     public void initialize() {
-        itemNameCol.setCellValueFactory(new PropertyValueFactory<PurchaseApprovals,String>("Item Name"));
-        amountCol.setCellValueFactory(new PropertyValueFactory<PurchaseApprovals,Double>("Amount"));
-        departmentCol.setCellValueFactory(new PropertyValueFactory<PurchaseApprovals,String>("Department"));
-        requestIdCol.setCellValueFactory(new PropertyValueFactory<PurchaseApprovals,Integer>("Request ID"));
-        statusCol.setCellValueFactory(new PropertyValueFactory<PurchaseApprovals,String>("Status"));
+        itemNameCol.setCellValueFactory(new PropertyValueFactory<>("ItemName"));
+        amountCol.setCellValueFactory(new PropertyValueFactory<>("Amount"));
+        departmentCol.setCellValueFactory(new PropertyValueFactory<>("Department"));
+        requestIdCol.setCellValueFactory(new PropertyValueFactory<>("RequestID"));
+        statusCol.setCellValueFactory(new PropertyValueFactory<>("Status"));
+
+        purchaseApprovalsObservableList.add(new PurchaseApprovals(1,"Pending","Laptop","IT",120000.0));
+        purchaseApprovalsObservableList.add(new PurchaseApprovals(2,"Pending","Printer","Admin",400000.0));
+        purchaseApprovalsObservableList.add(new PurchaseApprovals(3,"Approved","Air Conditioner","Operations",60000.0));
+        purchaseApprovalsObservableList.add(new PurchaseApprovals(4, "Rejected","Stationery","HR",8000.0));
+
+        purchaseApprovalTableView.setItems(purchaseApprovalsObservableList);
+
     }
 
     @javafx.fxml.FXML
     public void handleHomeButton(ActionEvent actionEvent) {
+        Jumper.jump((Node)actionEvent.getSource(),"rrhin.accountant","accountant-dashboard.fxml","Accountant Dashboard");
     }
 
     @javafx.fxml.FXML
     public void handleRejectButton(ActionEvent actionEvent) {
+        PurchaseApprovals selected = purchaseApprovalTableView.getSelectionModel().getSelectedItem();
+        if (selected !=null){
+            selected.setStatus("Rejected");
+            purchaseApprovalTableView.refresh();
+            purchaseApprovalTextArea.setText("Request " + selected.getRequestID() + "has been Rejected.");
+        }
     }
 
     @javafx.fxml.FXML
     public void handleApproveButton(ActionEvent actionEvent) {
+        PurchaseApprovals selected = purchaseApprovalTableView.getSelectionModel().getSelectedItem();
+        if(selected!=null){
+            selected.setStatus("Approved");
+            purchaseApprovalTableView.refresh();
+            purchaseApprovalTextArea.setText("Request " + selected.getRequestID()+" has been Approved.");
+        }
     }
 }
--- src/main/java/dsit/pmfinal/rrhin/accountant/model/PurchaseApprovals.java
+++ src/main/java/dsit/pmfinal/rrhin/accountant/model/PurchaseApprovals.java
@@ -1,10 +1,31 @@
 package dsit.pmfinal.rrhin.accountant.model;
 
 public class PurchaseApprovals {
+    private int requestID;
     private String purchaseApproval;
+    private String itemName;
+    private String department;
+    private double amount;
+    private String status;
 
-    public PurchaseApprovals(String purchaseApproval) {
+    public PurchaseApprovals(int requestID, String purchaseApproval, String itemName, String department, double amount, String status) {
+        this.requestID = requestID;
         this.purchaseApproval = purchaseApproval;
+        this.itemName = itemName;
+        this.department = department;
+        this.amount = amount;
+        this.status = status;
+    }
+
+    public PurchaseApprovals(int requestID, String pending, String laptop, String it, double amount) {
+    }
+
+    public int getRequestID() {
+        return requestID;
+    }
+
+    public void setRequestID(int requestID) {
+        this.requestID = requestID;
     }
 
     public String getPurchaseApproval() {
@@ -15,10 +36,47 @@ public class PurchaseApprovals {
         this.purchaseApproval = purchaseApproval;
     }
 
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     @Override
     public String toString() {
         return "PurchaseApprovals{" +
-                "purchaseApproval='" + purchaseApproval + '\'' +
+                "requestID=" + requestID +
+                ", purchaseApproval='" + purchaseApproval + '\'' +
+                ", itemName='" + itemName + '\'' +
+                ", department='" + department + '\'' +
+                ", amount=" + amount +
+                ", status='" + status + '\'' +
                 '}';
     }
 }
